933187375267c0c0fd7f1b1c409ee6f1c3844aa1
Mathilde Renard <mathilderenard626@gmail.com>
2022-03-25 11:28:57 +0100

    sprint 1

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+class Article:
 
+    def __init__(self,prix,code,nombre):
+        self.prix = prix
+        self.nombre = nombre
+        self.code = code
 
 
 if  __name__ =='__main__':
     print("Bienvenue")
+    article = Article(12,"CA",20)
+    print("l'article coute",article.prix,"euros")
+    print("Son code est : ",article.code)
+    print("Il y a en stock :",article.nombre,"articles")
